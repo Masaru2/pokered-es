1ff43accfa5769022303717fa37ac715603fd7b6
eientei <einstein95@users.noreply.github.com>
2022-07-10 17:33:16 +1200

    Minor cleanup

--- charmap.asm
+++ charmap.asm
@@ -194,10 +194,8 @@
 	charmap "<PK>",      $e1
 	charmap "<MN>",      $e2
 	charmap "-",         $e3
-
 	charmap "¿",         $e4
 	charmap "¡",         $e5
-
 	charmap "?",         $e6
 	charmap "!",         $e7
 	charmap ".",         $e8
